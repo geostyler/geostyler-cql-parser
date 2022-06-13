53d07b25f1b3dec78ad64908fb8638a0eae67de1
Andreas Schmitz <schmitz@terrestris.de>
2022-06-13 12:04:16 +0200

    Fix build

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
 module.exports = {
-  entry: "./src/CqlParser.ts",
+  entry: './src/CqlParser.ts',
   mode: 'development',
   output: {
-    filename: "CqlParser.js",
-    path: __dirname + "/browser",
-    library: "GeoStylerCqlParser"
+    filename: 'CqlParser.js',
+    path: __dirname + '/browser',
+    library: 'GeoStylerCqlParser'
   },
   resolve: {
-    extensions: [".ts", ".js", ".json"]
+    extensions: ['.ts', '.js', '.json'],
+    alias: {
+      fs: path.resolve('node_modules/graceful-fs')
+    }
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
         include: /src/,
-        loader: "awesome-typescript-loader"
+        loader: 'ts-loader'
       }
     ]
   }
